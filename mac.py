03b2c9e0a7da27105753b427023b07c4b6c09584
Kelly2607 <115078109+Kelly2607@users.noreply.github.com>
2022-10-05 16:30:04 +0530

    Update mac.py

--- mac.py
+++ mac.py
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-# Importing some important libraries
-import argparse
-import socket
-import sys
-import subprocess
-from os import system, name, popen
 from time import sleep
 
 amc_project_version = '2.0.0.6'
